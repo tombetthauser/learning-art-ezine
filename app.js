3e992103c8e105aa228bbae1806bb0b3c3878be2
tombetthauser <tombetthauser@gmail.com>
2020-10-25 02:21:28 -0700

    Add working live div shifter

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body");
 
-const blocks = [
-  [
+const boxes = [
+  [ // box 0 ------------------
     {
       tag: "h1",
       text: "Things I think I learned while I was learning and teaching art."
@@ -10,23 +10,51 @@ const blocks = [
       tag: "p",
       text: "The world's a fucked up place man..."
     }
+  ],
+  [ // box 1 ------------------
+    {
+      tag: "p",
+      text: "Once upon a midnight dreary, while I pondered, weak and weary. Over many a quaint and curious volume of forgotten lore. While I nodded, nearly napping, suddenly there came a tapping. As of some one gently rapping, rapping at my chamber door. 'Tis some visiter, I muttered, tapping at my chamber door. Only this, and nothing more. Ah, distinctly I remember it was in the bleak December. And each separate dying ember wrought its ghost upon the floor. Eagerly I wished the morrow;—vainly I had sought to borrow. From my books surcease of sorrow—sorrow for the lost Lenore. For the rare and radiant maiden whom the angels name Lenore. Nameless here for evermore. And the silken sad uncertain rustling of each purple curtain Thrilled me, filled me with fantastic terrors never felt before; So that now, to still the beating of my heart, I stood repeating Tis some visiter entreating entrance at my chamber door. Some late visiter entreating entrance at my chamber door. This it is, and nothing more."
+    }
   ]
 ]
 
-const spacer = div => {
-  const top = Math.random() * 100;
-  const left = Math.random() * 100;
-  const width = 200 + Math.random() * 300;
+const spacer = (div, squish = true) => {
+  const top = 50 + Math.random() * 100;
+  const left = 50 + Math.random() * 100;
+  let width;
+  
+  let string = `margin:${top}px 0 0 ${left}px;`
+  if (squish) {
+    width = 200 + Math.random() * 300;
+  } else {
+    width = div.style.width
+    width = width.slice(0, width.length - 2)
+  }
+  string += `width:${width}px;`
+  console.log(string)
 
-  div.setAttribute("style", `margin:${top}px 0 0 ${left}px; width:${width}px`);
+  div.setAttribute("style", string);
+}
+
+const shifter = () => {
+  const time = 200 + Math.floor(Math.random() * 500);
+
+  setTimeout(() => {
+    const boxes = document.querySelectorAll('.box');
+    const box = boxes[Math.floor(Math.random() * boxes.length)];
+
+    spacer(box, false)
+    shifter();
+  }, time);
 }
 
 const populate = () => {
-  blocks.forEach(block => {
+  boxes.forEach(box => {
     const div = document.createElement("div");
     div.setAttribute("class", "box")
     
-    block.forEach(object => {
+    box.forEach(object => {
       const ele = document.createElement(object.tag);
       ele.innerHTML = object.text;
       div.appendChild(ele)
@@ -35,6 +63,8 @@ const populate = () => {
     spacer(div);
     body.appendChild(div);
   })
+
+  shifter();
 }
 
 populate();
