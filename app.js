573559f510deb18d30d9c8428fcc90d39fc188b4
tombetthauser <tombetthauser@gmail.com>
2020-10-25 11:39:46 -0700

    Add image styling and some text

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 const body = document.querySelector("body");
 
+const boxes2 = [
+  [
+    {
+      tag: "h1",
+      text: "🤔 how are you <br>🤔 i miss you<br>🤔 are you ok?"
+    },
+    {
+      tag: 'ul',
+      text: "<li>erinkaczkowski</li>" +
+        "<li>tombetthauser</li>" +
+        "<li>danieltrejo</li>" +
+        "<li>justinamartino</li>" +
+        "<li>ivanbridges</li>" +
+        "<li>raebridges</li>" +
+        "<li>jeffersonjenson</li>" +
+        "<li>jenjefferson</li>" +
+        "<li>kikijohnson</li>" +
+        "<li>danielherwitt</li>" +
+        "<li>evannesbit</li>" +
+        "<li>michellfishugh</li>" +
+        "<li>francescalorusso</li>"
+    }  
+  ],
+  [
+    {
+      tag: "p",
+      text: "I miss my friends, I miss their art, I miss getting to see them in person. It's an oppourtunity to –I don't want to say connect, it's a meaningless word– it's a way for us to come together, to have our work come together. A way to see our community I guess? This is what our community looks like, this is what our friends look like. I don't know, that's probably all too sentimental..."
+    }
+  ]
+]
+
 const boxes = [
   [ // box 0 ------------------
     {
@@ -8,13 +39,29 @@ const boxes = [
     },
     {
       tag: "p",
-      text: "The world's a fucked up place man..."
+      text: "I studied painting at the San Francisco Art Institute and then Yale as a grad student. Then I taught painting and art history at some colleges across California for the better part of a decade. Recently I realized that teaching can't really support most people who do it and I started working as a software engineer. I'm still an artist though. These are just some things I learned that I wanted to write down before I forget them."
     }
   ],
   [ // box 1 ------------------
     {
+      tag: "img",
+      src: "https://www.donsmaps.com/images33/pebbleface.jpg"
+    },
+    {
+      tag: "h3",
+      text: "Art is the oldest thing ever."
+    },{
       tag: "p",
-      text: "Once upon a midnight dreary, while I pondered, weak and weary. Over many a quaint and curious volume of forgotten lore. While I nodded, nearly napping, suddenly there came a tapping. As of some one gently rapping, rapping at my chamber door. 'Tis some visiter, I muttered, tapping at my chamber door. Only this, and nothing more. Ah, distinctly I remember it was in the bleak December. And each separate dying ember wrought its ghost upon the floor. Eagerly I wished the morrow;—vainly I had sought to borrow. From my books surcease of sorrow—sorrow for the lost Lenore. For the rare and radiant maiden whom the angels name Lenore. Nameless here for evermore. And the silken sad uncertain rustling of each purple curtain Thrilled me, filled me with fantastic terrors never felt before; So that now, to still the beating of my heart, I stood repeating Tis some visiter entreating entrance at my chamber door. Some late visiter entreating entrance at my chamber door. This it is, and nothing more."
+      text: "Visual art and music are the oldest activities that distinguish us from other animals. We share these activities in common only with other archaic proto-humans such as Neanderthals. The earliest direct evidence of visual art and music hovers around 75-50,000 years ago and primarily takes the form of preserved cave paintings, sculptures and bone flutes. The oldest indirect evidence of pigment grinding and mixing however dates back to approximately 400,000 years ago, we just don't know what was being painted. It is plausible enough that paintings were just being made outdoors or as body art and because all paintings from this period were water soluable they naturally wouldn't have been preserved. Cave paintings are all water soluable too and are so sensitive they get damaged when people breath on them. Really it's amazing that we have any preserved pre-historic paintings at all. And indirect evidence that proto-humans valued objects purely for thier aesthetic qualities goes back even further, to one of the strangest art historical objects of all time, the Makapansgat Cobble. It's a small stone that appears to have a face that was carried around seemingly for several generations by a group of Australopithecus 2.8 million years ago. It's horrifying."
+    }
+  ],
+  [ // box 2 ------------------
+    {
+      tag: "h3",
+      text: "It began in Africa."
+    },{
+      tag: "p",
+      text: "Visual art and music are the oldest activities that distinguish us from other animals. We share these activities in common only with other archaic proto-humans such as Neanderthals. The earliest direct evidence of visual art and music hovers around 75-50,000 years ago and primarily takes the form of preserved cave paintings, sculptures and bone flutes. The oldest indirect evidence of pigment grinding and mixing however dates back to approximately 400,000 years ago, we just don't know what was being painted. It is plausible enough that paintings were just being made outdoors or as body art and because all paintings from this period were water soluable they naturally wouldn't have been preserved. Cave paintings are all water soluable too and are so sensitive they get damaged when people breath on them. Really it's amazing that we have any preserved pre-historic paintings at all."
     }
   ]
 ]
@@ -26,7 +73,7 @@ const spacer = (div, squish = true) => {
   
   let string = `margin:${top}px 0 0 ${left}px;`
   if (squish) {
-    width = 200 + Math.random() * 300;
+    width = 300 + Math.random() * 300;
   } else {
     width = div.style.width
     width = width.slice(0, width.length - 2)
@@ -56,7 +103,12 @@ const populate = () => {
     
     box.forEach(object => {
       const ele = document.createElement(object.tag);
-      ele.innerHTML = object.text;
+      if (object.tag === "img") {
+        alert(ele)
+        ele.setAttribute("src", object.src);
+      } else {
+        ele.innerHTML = object.text;
+      }
       div.appendChild(ele)
     })
 
