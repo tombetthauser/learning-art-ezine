1d5083367700bc407b3bcd4a0b0c60065c469297
tombetthauser <tombetthauser@gmail.com>
2020-10-27 21:30:23 -0700

    Start new text.js file for website building instructions

---
+++ text2.js
@@ -0,0 +1,19 @@
+const boxes = [
+  [
+    {
+      tag: "h1", text: "How to Make and Maintain a Free Artist's Website"
+    },{
+      tag: "p", text: "Step-by-step instructions for building and maintaining a free website for artists with no website building or coding experience."
+    }
+  ],[
+    {
+      tag: "img", src: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRZMBFlBpfKjp35TE7Bp1B89nNQezLfrlpeLQ&usqp=CAU"
+    },{
+      tag: "h2", text: "1) Make a website."
+    },{
+      tag: "p", text: "Let's get right into it and make the most basic website ever before we get to the boring stuff."
+    },{
+      tag: "p", text: "Open whatever your most basic text editor is. For Apple users this will be textEdit, you can find it by hitting <b>command + space bar</b> and then typing <b>textEdit</b> and then hitting enter. For Windows users it will probably be <b>notepad</b>, but regardless of your computer there are a lot of options. If you want something free and a little easier to use I would recommend <b>VS Code</b>.",
+    },
+  ],
+]
